92b14ccf6a2ee4e9044774e3c71734d71f5ad902
redtide <redtid3@gmail.com>
2019-10-31 16:49:12 +0100

    Fixing Travis CI build scripts.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  sudo add-apt-repository ppa:beineri/opt-qt-5.12.3-xenial -y
+  sudo add-apt-repository ppa:beineri/opt-qt-5.12.3-bionic -y
   sudo apt-get update -qq
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
   brew update
