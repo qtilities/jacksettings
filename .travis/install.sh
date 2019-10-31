17fe92be6ee1382a34d7f7ae8d06e83ebc925c41
redtide <redtid3@gmail.com>
2019-10-31 17:34:18 +0100

    Fixing Travis CI build scripts.

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,6 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get -y install libasound2-dev libjack-jackd2-dev libgl1-mesa-dev qt512base
-  source /opt/qt512/bin/qt512-env.sh
-  exit 0
+# source /opt/qt512/bin/qt512-env.sh
+# exit 0
 fi
