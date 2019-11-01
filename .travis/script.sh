05ab013bee43b2e45513fa7341d50b5b58f05fa1
redtide <redtid3@gmail.com>
2019-11-01 09:04:33 +0100

    Update Travis scripts.

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,9 +4,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100
   ${SHELL} --version && gcc -v && qmake -v 
 
-  qmake ${PROJECT_NAME}.pro -spec ${QMAKESPEC} CONFIG+=release PREFIX=/usr
+  qmake ${PROJECT_NAME}.pro -spec linux-g++ CONFIG+=release PREFIX=/usr
   make -j$(nproc)
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  qmake ${PROJECT_NAME}.pro -spec ${QMAKESPEC} CONFIG+=release
+  qmake ${PROJECT_NAME}.pro -spec macx-clang CONFIG+=release
   make -j$(sysctl -n hw.physicalcpu)
 fi
