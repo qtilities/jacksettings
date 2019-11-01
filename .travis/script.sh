b29dd99a9f57f67f56840ab71cb5b83cdfa2c326
redtide <redtid3@gmail.com>
2019-11-01 08:33:44 +0100

    Changed Qt project file case sensitive naming.

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,9 +4,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100
   gcc -v && g++ -v && $SHELL --version
 
-  qmake JackSettings.pro -spec linux-g++ CONFIG+=release PREFIX=/usr
+  qmake JACKSettings.pro -spec linux-g++ CONFIG+=release PREFIX=/usr
   make -j$(nproc)
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  qmake JackSettings.pro -spec macx-clang CONFIG+=release
+  qmake JACKSettings.pro -spec macx-clang CONFIG+=release
   make -j$(sysctl -n hw.physicalcpu)
 fi
